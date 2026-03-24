659ae7d5493c413ebf85753c4a82902c7a4ec0de
Tuomas Huusko <huusko.tuomas@gmail.com>
2026-03-24 18:17:07 +0200

    Init file

---
+++ src/planets.py
@@ -0,0 +1,20 @@
+from Planet import Planet
+from Sun import Sun
+import math
+
+SCALE = 0.1
+G = 1
+M = 1000
+
+sun = Sun(M, (200, 200))
+planets = []
+merkurius_distance = 10
+merkurius = Planet(
+    mass=10,
+    coords=(200, sun.coords[1] - merkurius_distance),
+    star=sun,
+    velocity=(math.sqrt(G * M * SCALE / (merkurius_distance)), 0),
+    id="Mercury",
+    scale=SCALE,
+    G=G,
+)
